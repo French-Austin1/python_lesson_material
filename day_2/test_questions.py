da77f7afba086625bea5ceb7d5958a28d700bf7b
French-Austin1 <austin.french@smoothstack.com>
2023-04-18 19:56:17 -0700

    Added some practice in the test_questions.py file

---
+++ day_2/test_questions.py
@@ -0,0 +1,48 @@
+'''
+This package contains functions that are either incomplete or have bugs.
+The purpose of this package is to help you practice debugging and testing.
+'''
+
+# Constants
+ITEMS = [{"name": "Phone", "price": 100, "quantity": 4},
+         {"name": "Laptop", "price": 1000, "quantity": 2},
+         {"name": "Keyboard", "price": 50, "quantity": 3}]
+
+# Complete the function below to return the total price of all items.
+def get_total_price():
+    pass
+
+# Complete the function below to get the quantity of a specific item.
+def get_quantity():
+    pass
+
+# Complete the function below to add a new item to the list.
+def add_new_item():
+    pass
+
+# Complete the function below to update the quantity of an item.
+def update_quantity():
+    pass
+
+# create a class to represent an item in the store
+class Item:
+    pass
+
+'''
+Create a class to represent a store.
+The store should have the following attributes:
+    name: name of the store
+    items: a list of items in the store
+    address: address of the store ( look at the faker library for help)
+    phone_number: phone number of the store ( look at the faker library for help)
+The store should have the following methods:
+    get_inventory: returns a list of all items in the store with their name and quantity
+    add_item: adds a new item to the store
+    remove_item: removes an item from the store
+    update_item_quantity: updates the quantity of an item in the store
+    sell_item: sells an item from the store and updates the quantity
+    get_total_revenue: returns the total revenue of the store
+'''
+
+class Store:
+    pass
